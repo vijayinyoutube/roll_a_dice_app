c0a60d36dd931b7f059b2cfa1ec3d3a6b20559e6
Vijay R <vijaythevirtue@gmail.com>
2022-03-24 02:50:43 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Overlay Loader',
+      title: 'Dice Challenge',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
